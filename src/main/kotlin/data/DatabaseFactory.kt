4af7bab021b1514a5186bde39e5363db7030fe7a
abdullah2808 <61104635+abdullah2808@users.noreply.github.com>
2025-10-27 15:25:49 -0700

    Updated DB factory to generate JDBC url

--- src/main/kotlin/data/DatabaseFactory.kt
+++ src/main/kotlin/data/DatabaseFactory.kt
@@ -3,11 +3,18 @@ package data
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
     fun init() {
+        val dbUrl = Env.DB_URL ?: error("DATABASE_URL not set")
+
+        // Parse URI
+        val uri = URI(dbUrl)
+        val jdbcUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}"
+        println(jdbcUrl)
         Database.connect(
-                url = Env.DB_URL,
+                url = jdbcUrl,
                 driver = "org.postgresql.Driver",
                 user = Env.DB_USER,
                 password = Env.DB_PASSWORD
